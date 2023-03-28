b5b81a34d5026fd99a86b9429301a605ae1eb555
divyaparyani67 <divyaparyani67@gmail.com>
2023-03-29 00:22:05 +0530

    fibonacce Sequence

---
+++ Math.js
@@ -0,0 +1,29 @@
+//fibonacci sequence
+
+function fibonacci(n) {
+  const fib = [0, 1]; // intialize an array with 2 numbers
+  for (let i = 2; i < n; i++) {
+    //populate the  remaining elements from the third element till n while satisfying condition that every number should be a sum of the previous two numbers
+
+    fib[i] = fib[i - 1] + fib[i - 2]; //adding two previous numbers  to get the sum
+  }
+  return fib;
+}
+
+//Big-O is O(n) linear
+
+console.log(fibonacci(1));
+console.log(fibonacci(8));
+console.log(fibonacci(29));
+
+//********************** */
+
+//factorial of a number
+
+function factorial(n) {
+  let result = 1;
+}
+
+console.log(factorial(0));
+console.log(factorial(5));
+console.log(factorial(9));
--- main.js
+++ main.js
@@ -19,6 +19,6 @@ function summation(n) {
 }
 
 //nested loops
-for(i = 1; i<= n; i++) {
-    for (j =1; j <= i; j++) 
-}
+for(i = 1; i<= n; i++) 
+    for (j =1; j <= i; j++); 
+
