4985d36fce8731e7e6ca9fd756d04e104228bf15
divyaparyani67 <divyaparyani67@gmail.com>
2023-03-29 01:16:23 +0530

    factorial of a number

--- Math.js
+++ Math.js
@@ -12,9 +12,9 @@ function fibonacci(n) {
 
 //Big-O is O(n) linear
 
-console.log(fibonacci(1));
-console.log(fibonacci(8));
-console.log(fibonacci(29));
+//console.log(fibonacci(1));
+//console.log(fibonacci(8));
+//console.log(fibonacci(29));
 
 //********************** */
 
@@ -22,8 +22,16 @@ console.log(fibonacci(29));
 
 function factorial(n) {
   let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
 }
 
+//Big-O is O(n) linear
+
 console.log(factorial(0));
+console.log(factorial(1));
 console.log(factorial(5));
-console.log(factorial(9));
+
+//Prime Number
