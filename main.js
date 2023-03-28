bcf16778245042501ce825d0eee7a507cb03cf5f
divyaparyani67 <divyaparyani67@gmail.com>
2023-03-28 22:49:13 +0530

    Big-O, Objects and arrays

---
+++ main.js
@@ -0,0 +1,24 @@
+//big-o time complexity
+
+function summation(n) {
+  let sum = 0; //executed only 1 time
+  for (let i = 1; i <= n; i++) {
+    // its just repeating the line 4
+    sum += i; // executes 4 times
+  }
+  return sum; // executes 1 time
+}
+
+// the total count is n+2 ( n is the number taken for input)
+// n = 100   100 + 2
+// n = 1000  1000 + 2
+
+//big-o calculation
+function summation(n) {
+  return (n * (n + 1)) / 2; //only executes  1 time
+}
+
+//nested loops
+for(i = 1; i<= n; i++) {
+    for (j =1; j <= i; j++) 
+}
