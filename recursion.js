b8c8eabbcfa0818448517021b55f1c50c379790a
divyaparyani67 <divyaparyani67@gmail.com>
2023-03-31 00:08:34 +0530

    recurion facorial problem

--- recursion.js
+++ recursion.js
@@ -10,6 +10,19 @@ console.log(recursiveFibionacci(1));
 console.log(recursiveFibionacci(0));
 console.log(recursiveFibionacci(6));
 
-//Big -O(2^n) performace is terrible it is not suitable in fibionacci sequence
+//Big O -O(2^n) performace is terrible it is not suitable in fibionacci sequence
 
 //factorial problem
+
+function recursiveFactorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * recursiveFactorial(n - 1);
+}
+
+console.log(recursiveFactorial(0));
+console.log(recursiveFactorial(1));
+console.log(recursiveFactorial(5));
+
+// Big O  = O(n)
