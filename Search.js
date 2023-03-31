bd3592932ded895b4fce99d3425b65a21063e3c0
divyaparyani67 <divyaparyani67@gmail.com>
2023-04-01 00:17:34 +0530

    binary search problem

--- Search.js
+++ Search.js
@@ -15,3 +15,26 @@ console.log(linearSearch([-5, 2, 10, 4, 6], 6));
 console.log(linearSearch([-5, 2, 10, 4, 6], 20));
 
 //Binary Search
+
+function binarySearch(arr, target) {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[middleIndex]) {
+      return middleIndex;
+    }
+    if (target < arr[middleIndex]) {
+      rightIndex = middleIndex - 1;
+    } else {
+      leftIndex = middleIndex + 1;
+    }
+  }
+  return -1;
+}
+
+// Big-O - O(logn)
+console.log(binarySearch([-5, 2, 4, 6, 10], 10));
+console.log(binarySearch([-5, 2, 4, 6, 10], 6));
+console.log(binarySearch([-5, 2, 4, 6, 10], 20));
