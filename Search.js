4e5944743d23b6393bd9f3c5d5c2441ea5cdcd07
divyaparyani67 <divyaparyani67@gmail.com>
2023-03-31 23:53:10 +0530

    linear search problem

---
+++ Search.js
@@ -0,0 +1,17 @@
+//linear search problem
+
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+//Big-O - O(n)
+console.log(linearSearch([-5, 2, 10, 4, 6], 10));
+console.log(linearSearch([-5, 2, 10, 4, 6], 6));
+console.log(linearSearch([-5, 2, 10, 4, 6], 20));
+
+//Binary Search
