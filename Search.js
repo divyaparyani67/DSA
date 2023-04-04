b158e6f6b65352db93db0f68c2d0f39c1079bd28
divyaparyani67 <divyaparyani67@gmail.com>
2023-04-04 05:55:35 +0530

    bubble sort

--- Search.js
+++ Search.js
@@ -60,6 +60,8 @@ function search(arr, target, leftIndex, rightIndex) {
   }
 }
 
+//Big-O - O(logn)
+
 console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10));
 console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 6));
 console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 20));
