d1e9562a60873b0b5ffba2d56f1afb0158c2bddd
divyaparyani67 <divyaparyani67@gmail.com>
2023-04-20 03:13:08 +0530

    prepend

--- Data Structures/linkedlist.js
+++ Data Structures/linkedlist.js
@@ -18,8 +18,41 @@ class LinkedList {
   getSize() {
     return this.size;
   }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is Empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+      while (curr) {
+        listValues += `${curr.value}`;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
 }
 
 const list = new LinkedList();
 console.log("List is empty", list.isEmpty());
 console.log("List Size", list.getSize());
+
+list.print();
+list.prepend(20);
+list.print();
+list.prepend(20);
+list.prepend(30);
+
+list.print();
